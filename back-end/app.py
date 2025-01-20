bf9f7f2eba830abb75f8364eba109872a95ba3f6
Arbin <abici20@epoka.edu.al>
2025-01-20 21:43:46 +0100

    Add Postgres integration

--- back-end/app.py
+++ back-end/app.py
@@ -1,11 +1,46 @@
 import os
-from flask import Flask
+from flask import Flask, request, jsonify
+import psycopg2
+from psycopg2 import sql
 from dotenv import load_dotenv
 load_dotenv()
 
 app = Flask(__name__)
 
+DB_HOST = "localhost"
+DB_NAME = "postgres"
+DB_USER = "postgres"
+DB_PASSWORD = "postgres"
+DB_PORT = 5432
+
 @app.route("/")
 def test():
     test_env = os.getenv("API_KEY")
     return f"<p>Key: {test_env}</p>"
+
+def get_db_connection():
+    conn = psycopg2.connect(
+        host = DB_HOST,
+        dbname = DB_NAME,
+        user = DB_USER,
+        password = DB_PASSWORD,
+        port = DB_PORT
+    )
+    return conn
+
+def create_users_table():
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute("""
+    CREATE TABLE IF NOT EXISTS users (
+        id SERIAL PRIMARY KEY,
+        username VARCHAR(80) UNIQUE NOT NULL,
+        email VARCHAR(120) UNIQUE NOT NULL,
+        password VARCHAR(120) NOT NULL
+    );
+    """)
+    conn.commit()
+    cur.close()
+    conn.close()
+
+create_users_table()
